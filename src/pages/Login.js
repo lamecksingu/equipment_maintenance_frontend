ddda7d4e7fb7b4a7c53bf56c5f455fe5cd5a245e
lamecksingu <singulameck9619@gmail.com>
2024-09-20 20:24:43 +0300

    Implementation of the login logic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode'; // Import jwtDecode for decoding tokens
 import '../styles/Login.css'; // A CSS file for created for styling
 
 const Login = () => {
@@ -13,8 +14,32 @@ const Login = () => {
   const handleLogin = async () => {
     try {
       const response = await axios.post('http://localhost:5000/api/auth/login', { email, password });
-      localStorage.setItem('token', response.data.token);
-      navigate('/dashboard'); // Redirect to dashboard after successful login
+      // localStorage.setItem('token', response.data.token);
+	    
+	    const token = response.data.token;
+
+	    // Decode the token to get the role
+	    const decodeToken = jwtDecode(token);
+	    const role = decodeToken.role;
+
+	    // store the role in local storage
+	    localStorage.setItem('userRole', role);
+
+	    // store the token in localStorage
+	    localStorage.setItem('userRole', role);
+
+     //  navigate('/dashboard'); // Redirect to dashboard after successful login
+
+	    // redirect to dashboard based on role
+	    if (role === 'admin') {
+		    navigate('/admin-dashboard');
+	    } else if (role === 'technician') {
+		    navigate('/technician-dashboard');
+	    } else if (role === 'user') {
+		    navigate('/user-dashboard');
+	    } else {
+		    navigate('/login');
+	    }
     } catch (error) {
       setError('Login failed. Please check your credentials and try again.');
     }
